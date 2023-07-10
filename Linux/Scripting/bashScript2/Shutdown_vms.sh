669f2190d42267ad88a0827a3754faa4ab467796
eznewo <eznewo@gmail.com>
2023-07-10 12:44:33 -0400

    Created script to turn off all the vms at once

---
+++ Linux/Scripting/bashScript2/Shutdown_vms.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+##### Script to turn off all your vms on the lab env't  #########
+
+# this will shutdown all the remote hosts using ansible shell module
+ansible all -m shell -a "shutdown now" -b
+
+# This one will shutdown the Ansible controle node
+sudo shutdown now 
